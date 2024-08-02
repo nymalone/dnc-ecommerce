552b7ccf50ddea1fbd1a463e0ced0788be6c1063
nymalone <malone.nykolle@gmail.com>
2024-08-02 10:03:41 -0300

    comment removed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-//TODO: explicar arquitetura de páginas - criar folders  - notion
-//TODO: fazer o app.js
-//TODO: explicar Router - notion
-//TODO: criar as rotas e as páginas básicas.
-//TODO: começar pela navbar SIMPLES e colocar no app.js...é um componente reutilizavel entre todas as pag.
-
-//TODO: após criar o mockup - notion
-
-//TODO: voltar pra navbar e fazer a navbar
-//TODO: criar product card
-//TODO: criar product list
-//TODO: voltar com ele pra home
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
